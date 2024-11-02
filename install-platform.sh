1ad44654159962ef058f03c7f67b01daaa6dfdda
Amine KAWANI <amine.kawani@outlook.com>
2024-11-02 13:07:00 +0100

    update

--- install-platform.sh
+++ install-platform.sh
@@ -48,48 +48,48 @@ while true; do
   sleep 5
 done
 
-# if [[ "${KUBRIX_TARGET_TYPE}" =~ ^KIND.* ]] ; then
-#   NAMESPACES="traefik backstage kargo grafana argocd keycloak komoplane kubecost falco minio velero velero-ui vault"
-#   for namespace in $NAMESPACES; do
-#     mkcert -cert-file ${namespace}-cert.pem -key-file ${namespace}-key.pem ${namespace}-127-0-0-1.nip.io 
-#     # Using kubectl create instead of apply to avoid last-applied annotation
-#     kubectl create namespace "$namespace" --dry-run=client -o yaml | kubectl apply -f -
-#     # # kargo needs a special secret name according to its helm chart
-#     if [ "${namespace}" = "kargo" ]; then
-#       kubectl delete secret kargo-api-ingress-cert -n ${namespace} --ignore-not-found=true
-#       kubectl create secret tls kargo-api-ingress-cert -n ${namespace} --cert=${namespace}-cert.pem --key=${namespace}-key.pem 
-#     else
-#       kubectl delete secret ${namespace}-server-tls -n ${namespace} --ignore-not-found=true
-#       kubectl create secret tls ${namespace}-server-tls -n ${namespace} --cert=${namespace}-cert.pem --key=${namespace}-key.pem
-#     fi
-#     rm ${namespace}-cert.pem ${namespace}-key.pem
-#   done
-# fi
+if [[ "${KUBRIX_TARGET_TYPE}" =~ ^KIND.* ]] ; then
+  NAMESPACES="traefik backstage kargo grafana argocd keycloak komoplane kubecost falco minio velero velero-ui vault"
+  for namespace in $NAMESPACES; do
+    mkcert -cert-file ${namespace}-cert.pem -key-file ${namespace}-key.pem ${namespace}-127-0-0-1.nip.io 
+    # Using kubectl create instead of apply to avoid last-applied annotation
+    kubectl create namespace "$namespace" --dry-run=client -o yaml | kubectl apply -f -
+    # # kargo needs a special secret name according to its helm chart
+    if [ "${namespace}" = "kargo" ]; then
+      kubectl delete secret kargo-api-ingress-cert -n ${namespace} --ignore-not-found=true
+      kubectl create secret tls kargo-api-ingress-cert -n ${namespace} --cert=${namespace}-cert.pem --key=${namespace}-key.pem 
+    else
+      kubectl delete secret ${namespace}-server-tls -n ${namespace} --ignore-not-found=true
+      kubectl create secret tls ${namespace}-server-tls -n ${namespace} --cert=${namespace}-cert.pem --key=${namespace}-key.pem
+    fi
+    rm ${namespace}-cert.pem ${namespace}-key.pem
+  done
+fi
 
-# helm template sx-argocd argo-cd \
-#   --repo https://argoproj.github.io/argo-helm \
-#   --namespace argocd \
-#   --set configs.cm.application.resourceTrackingMethod=annotation \
-#   -f bootstrap-argocd-values.yaml \
-#   | kubectl apply -f - 
+helm template sx-argocd argo-cd \
+  --repo https://argoproj.github.io/argo-helm \
+  --namespace argocd \
+  --set configs.cm.application.resourceTrackingMethod=annotation \
+  -f bootstrap-argocd-values.yaml \
+  | kubectl apply -f - 
 
-# # Check for pending pods in kube-system namespace
-# echo -e "${GREEN}Checking for pending pods in the argocd namespace...${NC}"
+# Check for pending pods in kube-system namespace
+echo -e "${GREEN}Checking for pending pods in the argocd namespace...${NC}"
 
-# while true; do
-#   PODS=$(kubectl get pods --namespace argocd --field-selector=status.phase=Pending -o wide)
+while true; do
+  PODS=$(kubectl get pods --namespace argocd --field-selector=status.phase=Pending -o wide)
   
-#   if [[ -z "$PODS" || "$PODS" == *"No resources found"* ]]; then
-#     echo "No pending pods found - cluster is ready."
-#     break
-#   else
-#     echo "Waiting for pods to be ready:"
-#     echo "$PODS"
-#   fi
-#   sleep 5
-# done
+  if [[ -z "$PODS" || "$PODS" == *"No resources found"* ]]; then
+    echo "No pending pods found - cluster is ready."
+    break
+  else
+    echo "Waiting for pods to be ready:"
+    echo "$PODS"
+  fi
+  sleep 5
+done
   
-# sleep 10
+sleep 10
 
 export ARGOCD_HOSTNAME=$(kubectl get ingress -o jsonpath='{.items[*].spec.rules[*].host}' -n argocd)
 INITIAL_ARGOCD_PASSWORD=$( kubectl get secret -n argocd argocd-initial-admin-secret -o=jsonpath={'.data.password'} | base64 -d )
